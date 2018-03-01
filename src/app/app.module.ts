9093239d4f338f369b553fbd27f05c968b65b5a2
himadri-repo <himadri.trainer@gmail.com>
2018-03-02 02:34:08 +0530

    Updated changes - not fully complete

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    exports: [
+        AppComponent,
+    ]
+})
+export class AppModule {
+
+}
