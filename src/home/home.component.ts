84e0e05de3446d44452530ba7bec0ec8ad850fdc
himadri-repo <himadri.trainer@gmail.com>
2018-03-14 01:50:18 +0530

    Updated html file

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -9,6 +9,10 @@ import { ActivatedRoute } from '@angular/router';
 export class HomeComponent implements OnInit {
     public showHome: boolean = true;
     public showKitchen: boolean = false;
+    public showSports: boolean = false;
+    public showWomenDress: boolean = false;
+    public showMenDress: boolean = false;
+    public showKidsDress: boolean = false;
 
     constructor(private router: ActivatedRoute) {
 
@@ -22,13 +26,54 @@ export class HomeComponent implements OnInit {
             if (params.id === '1') {
                 this.showHome = true;
                 this.showKitchen = false;
+                this.showSports = false;
+                this.showWomenDress = false;
+                this.showMenDress = false;
+                this.showKidsDress = false;
                 console.log('I am in 1');
             } else if (params.id === '2') {
                 this.showHome = false;
                 this.showKitchen = true;
+                this.showSports = false;
+                this.showWomenDress = false;
+                this.showMenDress = false;
+                this.showKidsDress = false;
                 console.log('I am in 2');
+            } else if (params.id === '3') {
+                this.showHome = false;
+                this.showKitchen = false;
+                this.showSports = true;
+                this.showWomenDress = false;
+                this.showMenDress = false;
+                this.showKidsDress = false;
+                console.log('I am in 3');
+            } else if (params.id === '41') {
+                this.showHome = false;
+                this.showKitchen = false;
+                this.showSports = false;
+                this.showWomenDress = true;
+                this.showMenDress = false;
+                this.showKidsDress = false;
+                console.log('I am in 41');
+            } else if (params.id === '42') {
+                this.showHome = false;
+                this.showKitchen = false;
+                this.showSports = false;
+                this.showWomenDress = false;
+                this.showMenDress = true;
+                this.showKidsDress = false;
+                console.log('I am in 42');
+            } else if (params.id === '43') {
+                this.showHome = false;
+                this.showKitchen = false;
+                this.showSports = false;
+                this.showWomenDress = false;
+                this.showMenDress = false;
+                this.showKidsDress = true;
+                console.log('I am in 43');
             }
-            console.log('Id: ' + params.id + ' showHome: ' + this.showHome + ' showKitchen: ' + this.showKitchen);
+        console.log('Id: ' + params.id + ' showHome: ' + this.showHome + ' showKitchen: '
+                + this.showKitchen + ' showSports: ' + this.showSports);
         });
     }
 }
